4ec97cd2d056338f153fea986e6cbe0971b34111
John LaDeur <jdladeur@gmail.com>
2019-11-18 10:24:41 -0600

    add list item function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
-//add list item
 
 let item = document.getElementById('input').value
-let ul = document.getElementById('list')
+let addBtn = document.getElementById('add-btn')
+
+//add list item
+addBtn.addEventListener('click', function(){
+    let ul = document.querySelector('list');
+    let li = document.createElement('li');
+    let item = document.getElementById('input').value
+    if (item == ''){
+        alert("Start your next item!")
+    } else {
+        li.appendChild(document.createTextNode(item))
+    }
+})
+
