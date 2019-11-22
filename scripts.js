8cf77925ace20450a39e966c20b2292e867e7374
John LaDeur <jdladeur@gmail.com>
2019-11-21 21:16:10 -0600

    added done button functionality

--- scripts.js
+++ scripts.js
@@ -3,10 +3,7 @@
   var itemList = document.getElementById("items");
   //Form Submit
   form.addEventListener("submit", addItem);
-  // Delete event
-  //itemList.addEventListener("click", removeItem);
-  //Mark Item as Done
-  //itemList.addEventListener("click", markAsDone);
+
   function addItem(e) {
     e.preventDefault();
     //Get input value
@@ -18,15 +15,24 @@
     li.appendChild(document.createTextNode(newItem));
     //Create Delete Button and Done Button
     var deleteBtn = document.createElement("button");
+    var doneBtn = document.createElement("button");
     // add click event listener
     deleteBtn.onclick = function(e) {
-      console.log('delete button was clicked!');
-      console.log(e.target);
-    }
+      console.log("delete button was clicked!");
+      li.style = "display: none";
+      
+    };
+
+    doneBtn.onclick = function(e) {
+      console.log("done button clicked!");
+      li.style.textDecoration = "line-through";
+      };
+
     deleteBtn.id = "delete";
     deleteBtn.className = "delete";
-    var doneBtn = document.createElement("button");
+    doneBtn.id = "done";
     doneBtn.className = "done";
+
     //append text node to buttons
     deleteBtn.appendChild(document.createTextNode("X"));
     doneBtn.appendChild(document.createTextNode("Done"));
@@ -38,17 +44,11 @@
   }
   //Delete Item
   // document.getElementById("delete").addEventListener("click", removeItem);
-  function removeItem(e) {
-    if (e.target.classList.contains("delete"));
-    var li = e.target.parentElement;
-    itemList.removeChild(li);
-  }
+  // function removeItem(e) {
+  // console.log('done button clicked!')
+  // }
   /* var deleteBtn = document.createElement("button");
   deleteBtn.className = "delete";
   var doneBtn = document.createElement("button");
   doneBtn.className = "done"; */
-  function markAsDone(e) {
-    if (e.target.classList.contains("done"));
-    var liDone = (e.target.parentElement.style.textDecoration = "line-through");
-  }
 })();
